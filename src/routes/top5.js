a58ea9af04ccbf1ca52582efb9a6731bb0e02838
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2026-09-02 19:30:59 -0300

    feat: implementa top5

---
+++ src/routes/top5.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import {
+  get,
+  create,
+  update,
+  remove,
+} from "../controllers/top5";
+
+import { authMiddleware } from "../middlewares/auth";
+
+const router = Router();
+
+router.get("/top5", authMiddleware(), get);
+
+router.post("/top5", authMiddleware(), create);
+
+router.put("/top5/:position", authMiddleware(), update);
+
+router.delete("/top5/:position", authMiddleware(), remove);
+
+export default router;
