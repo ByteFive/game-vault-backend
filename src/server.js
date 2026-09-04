6c20627672e44b5e2c98db61ddd53775790487cd
Alison Barbosa <alisonnbarbosaaa@gmail.com>
2026-09-04 14:41:28 -0300

    configura o cors

--- src/server.js
+++ src/server.js
@@ -1,31 +1,29 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import "dotenv/config.js";
-
-import { createHandler } from "graphql-http/lib/use/express";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import profileRoutes from "./routes/profile.js";
 import ratingRoutes from "./routes/rating.js";
 import gameRoutes from "./routes/game.js";
 import libraryRoutes from "./routes/library.js";
 import top5Routes from "./routes/top5.js";
-
+import { createHandler } from "graphql-http/lib/use/express";
 import { connectDatabase } from "./lib/db.js";
-
 import { schema } from "./graphql/schema.js";
 import { resolvers } from "./graphql/resolvers.js";
 import { createContext } from "./graphql/context.js";
 
+import "dotenv/config.js";
+
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(cookieParser());
 
 connectDatabase();
 
 // REST
-
 app.use(authRoutes);
 app.use(profileRoutes);
 app.use(ratingRoutes);
@@ -34,7 +32,6 @@ app.use(libraryRoutes);
 app.use(top5Routes);
 
 // GRAPHQL
-
 app.all(
   "/graphql",
   createHandler({
